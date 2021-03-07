f5f400ff4cc95173e227f06585c08125c8f60bc7
井上大志 <inouehiroshi@FL1-133-209-242-172.osk.mesh.ad.jp>
2021-03-07 15:13:06 +0900

    リンゴの落ち物ゲーム

---
+++ AppleCatch/Assets/GameDirector.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class GameDirector : MonoBehaviour
+{
+    GameObject timerText;
+    GameObject pointText;
+    GameObject ItemGenerator;
+    public float time = 60.0f;
+    int point = 0;
+    public bool GameSwitch
+    {
+        get { return gameSwitch; }
+        set
+        {
+            if (gameSwitch == false && value == true)
+            {
+                Debug.Log("Game Start!");
+            }
+
+            this.ItemGenerator.GetComponent<ItemGenerator>().ItemSwitch = value;
+            gameSwitch = value;
+        }
+    }
+
+    private bool gameSwitch;
+
+    public GameObject StartPanel;
+
+
+
+    public void GetApple(int score)
+    {
+        this.point += score;
+    }
+
+    public void GetBomb()
+    {
+        this.point /= 2;
+    }
+
+void Start()
+    {
+        this.timerText = GameObject.Find("Time");
+        this.pointText = GameObject.Find("Point");
+        this.ItemGenerator = GameObject.Find("ItemGenerator");
+        GameSwitch = false;
+
+
+
+        this.timerText.GetComponent<TextMeshProUGUI>().text = 0.0f.ToString("F1");
+        this.pointText.GetComponent<TextMeshProUGUI>().text = 0.ToString() + " point";
+    }
+    public class Parameter
+    {
+        public float startTime;
+        public float endTime;
+        public float interval;
+        public float speed;
+        public int rate;
+
+        public Parameter(float s, float e, float i, float sp, int r)
+        {
+            startTime = s;
+            endTime = e;
+            interval = i;
+            speed = sp;
+            rate = r;
+        }
+    }
+
+    private Parameter[] ParameterArray = new Parameter[5]
+    {
+        new Parameter(20.0f, 30.0f,1.0f, -0.03f, 2),
+        new Parameter(10.0f, 20.0f,0.8f, -0.04f, 4),
+        new Parameter(5.0f, 10.0f,0.8f, -0.05f, 6),
+        new Parameter(0.0f, 5.0f,0.7f, -0.04f, 3),
+        new Parameter(-10.0f, 0.0f,10000.0f, 0.0f, 0)
+    };
+
+
+
+
+
+
+    void Update()
+    {
+
+        if (GameSwitch == true)
+        {
+            this.time -= Time.deltaTime;
+
+            foreach (Parameter p in ParameterArray)
+            {
+                if (p.startTime <= this.time && this.time < p.endTime)
+                {
+                    this.ItemGenerator.GetComponent<ItemGenerator>().SetParameter(p.interval, p.speed, p.rate);
+                }
+
+            }
+
+            
+            if (this.time < 0)
+            {
+                this.time = 0;
+                GameSwitch = false;
+            }/*
+            else if (0 <= this.time && this.time < 5)
+            {
+                this.ItemGenerator.GetComponent<ItemGenerator>().SetParameter(0.9f, -0.04f, 3);
+            }
+            else if (5 <= this.time && this.time < 10)
+            {
+                this.ItemGenerator.GetComponent<ItemGenerator>().SetParameter(0.4f, -0.06f, 6);
+            }
+            else if (10 <= this.time && this.time < 20)
+            {
+                this.ItemGenerator.GetComponent<ItemGenerator>().SetParameter(0.7f, -0.04f, 4);
+            }
+            else if (20 <= this.time && this.time < 30)
+            {
+                this.ItemGenerator.GetComponent<ItemGenerator>().SetParameter(1.0f, -0.03f, 2);
+            }*/
+
+            this.timerText.GetComponent<TextMeshProUGUI>().text = this.time.ToString("F1");
+            this.pointText.GetComponent<TextMeshProUGUI>().text = this.point.ToString() + " point";
+        }
+    }
+
+    public void GameStart()
+    {
+        GameSwitch = true;
+        StartPanel.SetActive(false);
+    }
+
+
+}
---
+++ AppleCatch/Assets/ItemGenerator.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour
+{
+    public GameObject applePrefab;
+    public GameObject bananaPrefab;
+    public GameObject bombPrefab;
+    public bool ItemSwitch = false;
+    float span = 1.0f;
+    float delta = 0;
+    int ratio = 2;
+    float speed = -0.03f;
+
+    public void SetParameter(float span, float speed, int ratio)
+    {
+        this.span = span;
+        this.speed = speed;
+        this.ratio = ratio;
+    }
+
+    void Update()
+    {
+
+        if (ItemSwitch == true)
+        {
+            this.delta += Time.deltaTime;
+        }
+
+        if (this.delta > this.span && ItemSwitch == true)
+        {
+            this.delta = 0;
+            GameObject item;
+            GameObject Prefab;
+
+            int dice = Random.Range(1, 11);
+
+            if (dice <= this.ratio)
+            {
+                Prefab = bombPrefab;
+            }
+            else if (dice <= 5)
+            {
+                Prefab = bananaPrefab;
+            }
+            else
+            {
+                Prefab = applePrefab;
+            }
+
+            item = Instantiate(Prefab) as GameObject;
+
+            float x = Random.Range(-1, 2);
+            float z = Random.Range(-1, 2);
+            item.transform.position = new Vector3(x, 4, z);
+            item.GetComponent<ItemController>().dropSpeed = this.speed;
+        }
+    }
+}
